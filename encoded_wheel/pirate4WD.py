3b64b62557ff250c98611085565fb9837586f667
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-11-18 15:47:28 -0300

    callback function must have the pin as parameter :P

--- encoded_wheel/pirate4WD.py
+++ encoded_wheel/pirate4WD.py
@@ -4,10 +4,13 @@
 import RPi.GPIO as GPIO
 import time
 
-def actualizar():
+
+pin = 12
+
+def actualizar(pinx):
 	time0 = time.time()
 
-	GPIO.wait_for_edge(__pin_number, gpio.RISING)
+	GPIO.wait_for_edge(pinx, gpio.RISING)
 
 	deltaT = time.time() - time0
 	print("DTime = " + str(deltaT))
@@ -15,7 +18,6 @@ def actualizar():
 if __name__ == "__main__":
 	GPIO.setmode(GPIO.BOARD)
 
-	pin = 40
 	GPIO.setup(pin, GPIO.IN)	#pull_up_down=GPIO.PUD_DOWN
 	GPIO.add_event_detect(pin, GPIO.FALLING, callback=actualizar)	# add bounce time ?
 
