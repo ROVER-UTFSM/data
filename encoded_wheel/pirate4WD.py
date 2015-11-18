cb52e1052400d6d27a7cc280ac8e52b87efdf9e7
cramirezp <cristobal.ramirez@alumnos.usm.cl>
2015-11-18 15:40:27 -0300

    changing raw_input for time.sleep() loop

--- encoded_wheel/pirate4WD.py
+++ encoded_wheel/pirate4WD.py
@@ -19,4 +19,6 @@ if __name__ == "__main__":
 	GPIO.setup(pin, GPIO.IN)	#pull_up_down=GPIO.PUD_DOWN
 	GPIO.add_event_detect(pin, GPIO.FALLING, callback=actualizar)	# add bounce time ?
 
-	raw_input("")
+	while(True):
+		time.sleep(0.1)
+		my_time = time.time()
